f7165fa29dcd0adbae10f2ccc1e9d2f0bf21a863
Jordan <brasil@xpdeal.com>
2024-10-28 13:15:11 -0300

    new fork

--- components/Signature.js
+++ components/Signature.js
@@ -11,19 +11,8 @@ const Signature = () => {
         target="_blank"
         rel="noreferrer"
       >
-        <strong>Moisés Huaringa</strong>
+        <strong>XPDEAL</strong>
       </Typography>
-      . Source code on&nbsp;
-      <Typography
-        component="a"
-        href="https://github.com/moiseshp/dummy-logo-maker"
-        alt="link to github"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <strong>github</strong>
-      </Typography>
-      .
     </>
   );
 };
